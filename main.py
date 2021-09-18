2950960b9a2341862682b5e8a49c2bd7c3367b1d
aleph-oh <jhilton@mit.edu>
2021-09-18 12:23:49 -0400

    Make Hello World API route and add static dir

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
