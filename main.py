31ee4164927a4b782364d898cf78b4aaf5699e43
jhilton <jhilton@mit.edu>
2021-09-18 12:44:42 -0400

    Use templates dir for templating

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ async def root():
 
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
+templates = Jinja2Templates(directory="templates")
