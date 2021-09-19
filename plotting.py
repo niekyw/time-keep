020c44e2db966ee664f7acc75766ab1915cc2d6e
jhilton <jhilton@mit.edu>
2021-09-18 23:50:16 -0400

    Change database dependency in plotting.py

--- plotting.py
+++ plotting.py
@@ -1,30 +1,33 @@
-from bokeh.plotting import figure
 from bokeh.embed import components
-from datetime import datetime, timedelta, tzinfo
-from sqlite3 import Connection
+from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
-from fastapi import Depends, FastAPI, status
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import main
+from fastapi import Depends, APIRouter
 from math import pi
 
 import pandas as pd
 
 from bokeh.palettes import Accent
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure
 from bokeh.transform import cumsum
 
+import database_interactions
+import task_routes
 
+
+router = APIRouter(prefix="/plots")
+
+
+@router.get("/{username}", response_model=Optional[tuple[str, str]])
 def make_pie_chart(username: str,
                    min_time: Optional[datetime] = None,
                    max_time: Optional[datetime] = None,
                    categories: Optional[set[str]] = None,
-                   db=Depends(main.get_db)):
-    task_list = main.get_user_logs(username, min_time, max_time, categories, db=db)
-
+                   db = Depends(database_interactions.get_db)
+                   ):
+    task_list = task_routes.get_user_logs(username, min_time, max_time, categories, db=db)
+    if not task_list:
+        return None
     x = {task.name: task.length for task in task_list}
     if categories is not None:
         title_string = " & ".join(categories)
